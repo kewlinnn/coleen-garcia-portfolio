a7309970bc399b39701d00633af49e69fa4e3a3d
Coleen . <coleengarciado@gmail.com>
2025-06-26 01:04:13 +0800

    feat: add about me section

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,50 @@
     <section class="hero-section flex-align">
       <div class="hero-text">
         <h2 class="hero-greeting">Hello I'm <br />
-        <span class="hero-name linear-gradient-text">Coleen Garcia</span>
+        <span class="hero-name linear-gradient-text typewriter">Coleen Garcia</span>
       </h2>
       <h3 class="hero-job">Web Developer</h3>
       <p class="hero-description">I’m a web developer focused on front-end development with a strong foundation in Vue.js, React Native, and UI/UX design, I love turning ideas into interactive digital experiences. Whether I’m coding layouts or refining user flows in Figma, I aim to create intuitive solutions that leave a lasting impact.</p>
       </div>
-      <img src="../src/assets/images/coleen-image.png" alt="my picture" class="hero-image" />
+      <img src="../src/assets/images/coleen-image.png" alt="my picture" class="hero-image" draggable="false"/>
+    </section>
+    <img src="../src/assets/images/wave.svg" alt="wave transition" class="wave-transition" draggable="false" />
+
+    <section class="about-section">
+      <h2 class="section-title light">About me</h2>
+      <div class="about-content flex-align">
+        <div class="about-desc">
+          <p class="text-light">
+            My journey into web development began with a love for the arts — from drawing and design to crafting digital visuals. That passion naturally evolved into building user interfaces that are not just functional, but also visually engaging and intuitive.
+          </p>
+          <p class="text-light">
+            I specialize in creating responsive websites and mobile apps using Vue.js, React Native, and design tools like Figma and Canva. I thrive on transforming ideas into seamless digital experiences. Whether it’s designing wireframes or implementing interactive features, I aim to make every project both beautiful and usable.
+          </p>
+        </div>
+        <div class="about-education">
+          <p class="txt-lg bold">Bachelor of Science in Computer Science</p>
+          <p class="txt-md bold">Technological University of the Philippines</p>
+          <p class="txt-sm italic">Cum Laude</p>
+          <div class="flex-group">
+            <div class="educ-item flex-align flex-gap-1">
+              <CalendarIcon :color="colors.softBlack" />
+              <span>2020 - 2024</span>
+            </div>
+            <div class="educ-item flex-align flex-gap-1">
+              <LocationIcon :color="colors.softBlack" />
+              <span>Manila, Philippines</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   </main>
 </template>
 
 <script setup>
+import CalendarIcon from './components/icons/CalendarIcon.vue';
 import { ref } from 'vue';
+import LocationIcon from './components/icons/LocationIcon.vue';
 
 const navbarItems = ref([
   { name: 'Home', link: '/' },
@@ -28,6 +60,16 @@ const navbarItems = ref([
   { name: 'Projects', link: '/projects' },
   { name: 'Contact', link: '/contact' }
 ])
+
+const colors = {
+  primary: "#f4f4f4",
+  secondary: "#A04747",
+  accent: "#D8A25E",
+  accentLight: "#EEDF7A",
+  softBlack: "#343131",
+  darkBlack: "#1a1a1a",
+  brightWhite: "#ffffff"
+}
 </script>
 
 <style scoped>
