e10e1a8118cbe7c575f41e59f8ed49d04a0b6ed7
Coleen . <coleengarciado@gmail.com>
2025-06-25 22:18:08 +0800

    feat: add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
-<script setup>
-</script>
-
 <template>
   <header>
+    <nav class="navbar">
+        <span class="navbar-item" v-for="item in navbarItems" key="item.name">{{ item.name }}</span>
+    </nav>
   </header>
 
   <main>
   </main>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const navbarItems = ref([
+  { name: 'Home', link: '/' },
+  { name: 'About', link: '/about' },
+  { name: 'Projects', link: '/projects' },
+  { name: 'Contact', link: '/contact' }
+])
+</script>
 
 <style scoped>
 </style>
